953e2be344d7ccd3b3dc94f77214c1653b458ab8
Leandro Lerena <leandro@lerena.ch>
2025-06-08 15:14:03 +0200

    wrap starter code into a main method to enable integration testing

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -10,17 +10,22 @@ from app.ui.components.add_new_post import show_add_new_post
 from app.ui.components.posts_history import show_posts_history
 from app.ui.components.configure_user_name import show_configure_user_name
 
-# Creates and renders the title of the app
-st.title("Microblogger")
+def main():
+    """Main function to run the Streamlit app."""
+    # Creates and renders the title of the app
+    st.title("Microblogger")
 
-# Creates and renders a user configuration component
-show_configure_user_name()
+    # Creates and renders a user configuration component
+    show_configure_user_name()
 
-# Creates and renders the post history
-show_posts_history()
+    # Creates and renders the post history
+    show_posts_history()
 
-# Creates and renders a component to add a new post
-show_add_new_post()
+    # Creates and renders a component to add a new post
+    show_add_new_post()
 
-# This makes sure the app refreshes every 2 seconds to show newly added posts
-st_autorefresh(interval=2000, limit=None, key="auto-refresh")
+    # This makes sure the app refreshes every 2 seconds to show newly added posts
+    st_autorefresh(interval=2000, limit=None, key="auto-refresh")
+
+if __name__ == "__main__":
+    main()
