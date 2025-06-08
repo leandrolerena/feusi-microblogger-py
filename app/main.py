633d2b9d50ed15f7238f03f914d7b8172d6f17d4
Leandro Lerena <leandro@lerena.ch>
2025-06-08 18:10:13 +0200

    rename project from microblogger to app and add entry point for the application; get relative path of the entry file

--- app/main.py
+++ app/main.py
@@ -2,15 +2,20 @@
 This script is the entry point for the Streamlit application.
 It calls the Streamlit bootstrap function to run the app.
 """
+import inspect
+import app.streamlit_app
 
 import streamlit.web.bootstrap
 
-APP_PATH = "streamlit_app.py"
+file_path = inspect.getfile(app.streamlit_app)
 
-if __name__ == "__main__":
+def main():
     streamlit.web.bootstrap.run(
-        APP_PATH,
+        file_path,
         False,  # Command-line args as a string, e.g. "--server.port 8502"
         [],
         {},
     )
+
+if __name__ == "__main__":
+    main()
