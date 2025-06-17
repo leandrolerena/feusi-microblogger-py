6838c34c204b16a296ea52899de5841ddde08bca
Leandro <leandro@lerena.ch>
2025-06-17 14:21:21 +0000

    Fix numbering of posts to start at 1 instead of 0

--- app/ui/components/posts_history.py
+++ app/ui/components/posts_history.py
@@ -8,4 +8,4 @@ def show_posts_history():
     st.subheader("Posts")
     for i, post in enumerate(all_posts):
         timestamp_str = post.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        st.write(f"{i}. - {str(post)}")
+        st.write(f"{i+1}. - {str(post)}")
